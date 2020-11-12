150dd94be47028ecc5ac4f61dca1f0970e8af2ff
Reptar <Reptar25@github.com>
2020-11-12 13:28:52 -0800

    Added Main class

---
+++ MudPitBot/src/main/java/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Main {
+
+	/*
+	 * TODO: Create bot on https://discord.com/developers/applications/ to get a TokenKey to login to discord with.
+	 */
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
