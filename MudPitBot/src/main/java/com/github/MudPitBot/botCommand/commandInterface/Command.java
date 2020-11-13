5b6947a78793ac0679d2d44e6e7ce44f933d1bf3
reptar25 <bailey.kevin89@gmail.com>
2020-11-13 09:44:38 -0800

    Moved comments

--- MudPitBot/src/main/java/com/github/MudPitBot/botCommand/commandInterface/Command.java
+++ MudPitBot/src/main/java/com/github/MudPitBot/botCommand/commandInterface/Command.java
@@ -2,6 +2,10 @@ package com.github.MudPitBot.botCommand.commandInterface;
 
 import com.github.MudPitBot.botCommand.BotReceiver;
 
+/*
+ * Implementation of the Command design pattern.
+ */
+
 public abstract class Command implements CommandInterface {
 	
 	protected BotReceiver receiver;
--- MudPitBot/src/main/java/com/github/MudPitBot/botCommand/commandInterface/CommandInterface.java
+++ MudPitBot/src/main/java/com/github/MudPitBot/botCommand/commandInterface/CommandInterface.java
@@ -2,10 +2,6 @@ package com.github.MudPitBot.botCommand.commandInterface;
 
 import discord4j.core.event.domain.message.MessageCreateEvent;
 
-/*
- * Implementation of the Command design pattern.
- */
-
 public interface CommandInterface {
 
 	public void execute(MessageCreateEvent event);
