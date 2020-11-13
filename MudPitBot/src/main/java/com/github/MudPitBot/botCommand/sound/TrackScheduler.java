058dfac0bd290e2f217ee1733f85d60db63d653e
reptar25 <bailey.kevin89@gmail.com>
2020-11-13 15:38:24 -0800

    Added some logging messages

--- MudPitBot/src/main/java/com/github/MudPitBot/botCommand/sound/TrackScheduler.java
+++ MudPitBot/src/main/java/com/github/MudPitBot/botCommand/sound/TrackScheduler.java
@@ -1,12 +1,18 @@
 package com.github.MudPitBot.botCommand.sound;
 
+import com.github.MudPitBot.botCommand.BotReceiver;
 import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
 import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 
+import reactor.util.Logger;
+import reactor.util.Loggers;
+
 public final class TrackScheduler implements AudioLoadResultHandler {
+	
+	private static final Logger LOGGER = Loggers.getLogger(TrackScheduler.class);
 
 	private final AudioPlayer player;
 
@@ -23,15 +29,18 @@ public final class TrackScheduler implements AudioLoadResultHandler {
 	@Override
 	public void playlistLoaded(final AudioPlaylist playlist) {
 		// LavaPlayer found multiple AudioTracks from some playlist
+		LOGGER.info("Playlist loaded");
 	}
 
 	@Override
 	public void noMatches() {
 		// LavaPlayer did not find any audio to extract
+		LOGGER.info("Did not find any audio to extract");
 	}
 
 	@Override
 	public void loadFailed(final FriendlyException exception) {
 		// LavaPlayer could not parse an audio source for some reason
+		LOGGER.info("Could not parse an audio source for some reason");
 	}
 }
