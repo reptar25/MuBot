797f6a5d998773aef4d54497e4fcef666b1a7e92
reptar25 <bailey.kevin89@gmail.com>
2020-11-13 14:48:34 -0800

    Added LavaPlayer

--- MudPitBot/src/main/java/com/github/MudPitBot/main/Main.java
+++ MudPitBot/src/main/java/com/github/MudPitBot/main/Main.java
@@ -1,10 +1,35 @@
 package com.github.MudPitBot.main;
 
+import com.github.MudPitBot.botCommand.sound.LavaPlayerAudioProvider;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
+import com.sedmelluq.discord.lavaplayer.track.playback.NonAllocatingAudioFrameBuffer;
+
 import discord4j.core.DiscordClientBuilder;
 import discord4j.core.GatewayDiscordClient;
+import discord4j.voice.AudioProvider;
 
 public class Main {
+	
+	public static AudioProvider provider;
+	public static AudioPlayerManager playerManager;
+	public static AudioPlayer player;
+	
 	public static void main(String[] args) {
+		
+		// Creates AudioPlayer instances and translates URLs to AudioTrack instances
+		playerManager = new DefaultAudioPlayerManager();
+		// This is an optimization strategy that Discord4J can utilize. It is not important to understand
+		playerManager.getConfiguration().setFrameBufferFactory(NonAllocatingAudioFrameBuffer::new);
+		// Allow playerManager to parse remote sources like YouTube links
+		AudioSourceManagers.registerRemoteSources(playerManager);
+		// Create an AudioPlayer so Discord4J can receive audio data
+		player = playerManager.createPlayer();
+		// We will be creating LavaPlayerAudioProvider in the next step
+		provider = new LavaPlayerAudioProvider(player);
+		
 		String token = System.getenv("token");
 		if (token == null)
 			token = args[0];
