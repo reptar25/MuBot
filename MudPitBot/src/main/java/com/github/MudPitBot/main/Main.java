44a3009b3692141be787ff86e96aa7f2044690ef
reptar25 <bailey.kevin89@gmail.com>
2020-11-13 01:39:56 -0800

    Removed TOOD

--- MudPitBot/src/main/java/com/github/MudPitBot/main/Main.java
+++ MudPitBot/src/main/java/com/github/MudPitBot/main/Main.java
@@ -6,12 +6,6 @@ import discord4j.core.DiscordClientBuilder;
 import discord4j.core.GatewayDiscordClient;
 
 public class Main {
-
-	/*
-	 * TODO: Create bot on https://discord.com/developers/applications/ to get a
-	 * TokenKey to login to discord with.
-	 */
-
 	public static void main(String[] args) {
 		final GatewayDiscordClient client = DiscordClientBuilder.create(args[0]).build().login().block();
 
