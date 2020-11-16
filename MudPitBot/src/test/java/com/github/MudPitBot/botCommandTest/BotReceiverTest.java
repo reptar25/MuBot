736291b851c252995a948f1a8db7bcb012b47fca
reptar25 <bailey.kevin89@gmail.com>
2020-11-16 14:48:07 -0800

    Fixed mock tests

--- MudPitBot/src/test/java/com/github/MudPitBot/botCommandTest/BotReceiverTest.java
+++ MudPitBot/src/test/java/com/github/MudPitBot/botCommandTest/BotReceiverTest.java
@@ -6,9 +6,10 @@ import com.github.MudPitBot.botCommand.commandImpl.Commands;
 import com.github.MudPitBot.botCommand.commandInterface.Command;
 
 import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Guild;
 import discord4j.core.object.entity.Message;
-import discord4j.discordjson.json.ImmutableMessageData;
-import discord4j.discordjson.json.MessageData;
+import discord4j.core.object.entity.channel.MessageChannel;
+import reactor.core.publisher.Mono;
 
 import static org.mockito.Mockito.*;
 
@@ -16,9 +17,21 @@ class BotReceiverTest {
 
 	@Mock
 	MessageCreateEvent mockEvent = mock(MessageCreateEvent.class);
-	
+
 	@Mock
 	Message mockMessage = mock(Message.class);
+
+	@Mock
+	MessageChannel mockChannel = mock(MessageChannel.class);
+
+	@Mock
+	Guild mockGuild = mock(Guild.class);
+
+	@Mock
+	Mono<MessageChannel> monoChannel = mock(Mono.class);
+
+	@Mock
+	Mono<Guild> monoGuild = mock(Mono.class);
 
 	@Test
 	void nullEvent() {
@@ -43,22 +56,25 @@ class BotReceiverTest {
 			c.execute(mockEvent);
 		}
 	}
-	
-//	@Test
-//	void nullGuild() {
-//		when(mockEvent.getGuild().block()).thenReturn(null);
-//		for (Command c : Commands.COMMANDS.values()) {
-//			c.execute(mockEvent);
-//		}
-//	}
-//	
-//	@Test
-//	void nullChannel() {
-//		when(mockEvent.getMessage()).thenReturn(mockMessage);
-//		when(mockMessage.getChannel().block()).thenReturn(null);
-//		for (Command c : Commands.COMMANDS.values()) {
-//			c.execute(mockEvent);
-//		}
-//	}
+
+	@Test
+	void nullGuild() {
+		when(mockEvent.getGuild()).thenReturn(monoGuild);
+		when(mockEvent.getGuild().block()).thenReturn(mockGuild);
+		for (Command c : Commands.COMMANDS.values()) {
+			c.execute(mockEvent);
+		}
+	}
+
+	@Test
+	void nullChannel() {
+		// when(mockEvent.getMessage()).thenReturn(mockMessage);
+		when(mockEvent.getMessage()).thenReturn(mockMessage);
+		when(mockEvent.getMessage().getChannel()).thenReturn(monoChannel);
+		when(mockEvent.getMessage().getChannel().block()).thenReturn(mockChannel);
+		for (Command c : Commands.COMMANDS.values()) {
+			c.execute(mockEvent);
+		}
+	}
 
 }
