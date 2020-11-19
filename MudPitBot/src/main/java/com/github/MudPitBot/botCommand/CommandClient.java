43a5ea75e4041fec736b72929c04641f173b3b3b
reptar25 <bailey.kevin89@gmail.com>
2020-11-18 17:20:42 -0800

    Cleaned up client code

--- MudPitBot/src/main/java/com/github/MudPitBot/botCommand/CommandClient.java
+++ MudPitBot/src/main/java/com/github/MudPitBot/botCommand/CommandClient.java
@@ -57,80 +57,116 @@ public class CommandClient {
 					if (event.getMessage() != null && event.getMessage().getContent() != null) {
 						// 3.1 Message.getContent() is a String
 						final String content = event.getMessage().getContent().toLowerCase();
-						// System.out.println("MESSAGE CREATED: "+content);
-						StringBuilder sb = new StringBuilder("New message created: ");
 
-						Member member = event.getMember().orElse(null);
-						// add the user name and put the message in quotes
-						if (member != null) {
-							sb.append(member.getUsername());
-						}
-						else {
-							sb.append(event.getMessage().getAuthor().orElse(null).getUsername());
-						}
-						sb.append(" - \"").append(content).append("\"");
-						LOGGER.info(sb.toString());
-						for (final Entry<String, Command> entry : Commands.entries()) {
-							// We will be using ! as our "prefix" to any command in the system.
-							if (content.startsWith('!' + entry.getKey().toLowerCase())) {
-								executor.executeCommand(entry.getValue(), event);
-								break;
-							}
-						}
+						// print out new message to logs
+						logMessage(event, content);
+
+						// process new message to check for commands
+						processMessage(event, content);
 					}
 				});
 
+		/*
+		 * Add listener for members joining/changing voice channels.
+		 */
 		client.getEventDispatcher().on(VoiceStateUpdateEvent.class).subscribe(event -> {
 
-			if (!CommandReceiver.muteToggle) {
-				return;
-			}
-
-			// if its a bot who joined, do not execute any more code
-			if (event.getCurrent().getMember().block().isBot()) {
-				return;
-			}
-
-			if (event.getCurrent().getChannelId() == null) {
-				return;
-			}
-			// get channel id of user who joined
-			long userChannelId = event.getCurrent().getChannelId().orElse(null).asLong();
-			// get bot channel id
-			// long botChannelId =
-			// client.getSelf().block().asMember(client.getSelfId()).block().getVoiceState().block()
-			// .getChannelId().orElse(null).asLong();
-
-			// if user join same channel as mute channel
-			if (userChannelId == CommandReceiver.muteChannelId) {
-				if (event.isJoinEvent() || event.isMoveEvent()) {
-					// if the mute toggle is enabled
-					if (CommandReceiver.muteToggle)
-						// mute the member that just joined
-						event.getCurrent().getMember().block().edit(spec -> spec.setMute(true)).block();
-					LOGGER.info("Muting " + event.getCurrent().getUser().block().getUsername());
-				}
-			}
-			// if user joined another channel, make sure they arent muted still
-			else {
-				VoiceState oldVoiceState = event.getOld().orElse(null);
-				if (oldVoiceState != null) {
-
-					if (event.isMoveEvent() || event.isMoveEvent()) {
-						long oldChannelId = oldVoiceState.getChannelId().orElse(null).asLong();
-						// if the channel their are leaving is the channel that was muted
-						if (oldChannelId == CommandReceiver.muteChannelId) {
-							// if the mute toggle is enabled
-							if (CommandReceiver.muteToggle)
-								// unmute the leaving member
-								event.getCurrent().getMember().block().edit(spec -> spec.setMute(false)).block();
-							LOGGER.info("Unmuting " + event.getCurrent().getUser().block().getUsername());
-						}
-					}
-				}
-
-			}
+			// Checks whether a member should be muted on joining a voice channel and mutes them if so
+			muteOnJoin(event);
 		});
 	};
 
+	/**
+	 * Checks if the new voice channel is the channel the bot currently has muted, and mutes the member if it is
+	 * 
+	 * @param event event of the channel change
+	 */
+	private void muteOnJoin(VoiceStateUpdateEvent event) {
+
+		if (!CommandReceiver.muteToggle) {
+			return;
+		}
+
+		// if its a bot who joined, do not execute any more code
+		if (event.getCurrent().getMember().block().isBot()) {
+			return;
+		}
+
+		if (event.getCurrent().getChannelId() == null) {
+			return;
+		}
+		// get channel id of user who joined
+		long userChannelId = event.getCurrent().getChannelId().orElse(null).asLong();
+
+		// if user join same channel as mute channel
+		if (userChannelId == CommandReceiver.muteChannelId) {
+			if (event.isJoinEvent() || event.isMoveEvent()) {
+				// if the mute toggle is enabled
+				if (CommandReceiver.muteToggle)
+					// mute the member that just joined
+					event.getCurrent().getMember().block().edit(spec -> spec.setMute(true)).block();
+				LOGGER.info("Muting " + event.getCurrent().getUser().block().getUsername());
+			}
+		}
+		// if user joined another channel, make sure they arent muted still
+		else {
+			VoiceState oldVoiceState = event.getOld().orElse(null);
+			if (oldVoiceState != null) {
+
+				if (event.isMoveEvent() || event.isMoveEvent()) {
+					long oldChannelId = oldVoiceState.getChannelId().orElse(null).asLong();
+					// if the channel their are leaving is the channel that was muted
+					if (oldChannelId == CommandReceiver.muteChannelId) {
+						// if the mute toggle is enabled
+						if (CommandReceiver.muteToggle)
+							// unmute the leaving member
+							event.getCurrent().getMember().block().edit(spec -> spec.setMute(false)).block();
+						LOGGER.info("Unmuting " + event.getCurrent().getUser().block().getUsername());
+					}
+				}
+			}
+		}
+	}
+
+	/**
+	 * Processes the message to check for commands and execute those commands
+	 * 
+	 * @param event   event of the message
+	 * @param content content of the message
+	 */
+	private void processMessage(MessageCreateEvent event, String content) {
+		// split content at ! to allow for compound commands (more than 1 command in 1
+		// message)
+		String[] commands = content.split("!");
+		for (final String command : commands) {
+			for (final Entry<String, Command> entry : Commands.entries()) {
+				// We will be using ! as our "prefix" to any command in the system.
+				if (command.startsWith('!' + entry.getKey().toLowerCase())) {
+					executor.executeCommand(entry.getValue(), event);
+					break;
+				}
+			}
+		}
+	}
+
+	/**
+	 * Formats and logs the message content and author
+	 * 
+	 * @param event   even of the message
+	 * @param content content of the message
+	 */
+	private void logMessage(MessageCreateEvent event, String content) {
+		StringBuilder sb = new StringBuilder("New message created: ");
+
+		Member member = event.getMember().orElse(null);
+
+		// add the user name and put the message in quotes
+		if (member != null) {
+			sb.append(member.getUsername());
+		} else {
+			sb.append(event.getMessage().getAuthor().orElse(null).getUsername());
+		}
+		sb.append(" - \"").append(content).append("\"");
+		LOGGER.info(sb.toString());
+	}
 }
