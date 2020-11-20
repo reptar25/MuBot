9a28bddfc07008c7bda2300e4159d601e2fae950
reptar25 <bailey.kevin89@gmail.com>
2020-11-20 14:11:42 -0800

    Added comment

--- MudPitBot/src/main/java/com/github/MudPitBot/botCommand/commandInterface/Command.java
+++ MudPitBot/src/main/java/com/github/MudPitBot/botCommand/commandInterface/Command.java
@@ -7,18 +7,24 @@ import com.github.MudPitBot.botCommand.CommandReceiver;
  */
 
 public abstract class Command implements CommandInterface {
-	
+
 	protected CommandReceiver receiver;
 	protected String commandTrigger;
 
 	public Command(CommandReceiver receiver) {
 		this.receiver = receiver;
 	}
+
+	/*
 	
-	
+	 */
 	/**
-	 * This enforces users to implement what the command trigger should be when subclassing. If it was just a varible subclasses inherited then
-	 * there would be no way to enforce it being set.
+	 * This enforces users to implement what the command trigger should be when
+	 * making a subclass. If we just used a protected variable then there would be
+	 * no way to enforce it being set. Command names should always be in lower-case
+	 * here since we do .toLowerCase() when checking the command to make them non
+	 * case sensitive.
+	 * 
 	 * @return the string literal of what triggers this command.
 	 */
 	public abstract String getCommandTrigger();
