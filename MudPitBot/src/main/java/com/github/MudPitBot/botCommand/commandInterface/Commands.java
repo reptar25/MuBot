14fa7106736ac3cfefa5beb555629ad69e03ec6d
reptar25 <bailey.kevin89@gmail.com>
2020-11-20 13:27:40 -0800

    Removed old commands map comments

--- MudPitBot/src/main/java/com/github/MudPitBot/botCommand/commandInterface/Commands.java
+++ MudPitBot/src/main/java/com/github/MudPitBot/botCommand/commandInterface/Commands.java
@@ -25,20 +25,6 @@ public abstract class Commands {
 	 * when checking the command to make them non case sensitive.
 	 */
 	static {
-//		COMMANDS.put("join", new JoinVoiceCommand(RECEIVER));
-//		COMMANDS.put("echo", new EchoCommand(RECEIVER));
-//		COMMANDS.put("leave", new LeaveVoiceCommand(RECEIVER));
-//		COMMANDS.put("roll", new RollCommand(RECEIVER));
-//		COMMANDS.put("play", new PlayCommand(RECEIVER));
-//		COMMANDS.put("volume", new VolumeCommand(RECEIVER));
-//		COMMANDS.put("stop", new StopCommand(RECEIVER));
-//		COMMANDS.put("skip", new SkipCommand(RECEIVER));
-//		COMMANDS.put("next", new SkipCommand(RECEIVER));
-//		COMMANDS.put("mute", new MuteCommand(RECEIVER));
-//		COMMANDS.put("clear", new ClearCommand(RECEIVER));
-//		COMMANDS.put("viewqueue", new ViewQueueCommand(RECEIVER));
-//		COMMANDS.put("nowplaying", new NowPlayingCommand(RECEIVER));
-//		COMMANDS.put("poll", new PollCommand(RECEIVER));
 		buildCommandMap();
 	}
 
