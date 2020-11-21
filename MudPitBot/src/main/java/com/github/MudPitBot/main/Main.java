c99951fabe2ff3a9aadd1002d9dfa51c7db1b5b8
reptar25 <bailey.kevin89@gmail.com>
2020-11-21 10:29:48 -0800

    Fallback incase PORT env variable isn't present

--- MudPitBot/src/main/java/com/github/MudPitBot/main/Main.java
+++ MudPitBot/src/main/java/com/github/MudPitBot/main/Main.java
@@ -2,11 +2,14 @@ package com.github.MudPitBot.main;
 
 import com.github.MudPitBot.Heroku.HerokuServer;
 import com.github.MudPitBot.botCommand.CommandClient;
-
 import discord4j.core.DiscordClientBuilder;
 import discord4j.core.GatewayDiscordClient;
+import reactor.util.Logger;
+import reactor.util.Loggers;
 
 public class Main {
+
+	private static final Logger LOGGER = Loggers.getLogger(Main.class);
 
 	public static void main(String[] args) {
 		String token = args[0];
@@ -15,8 +18,14 @@ public class Main {
 
 		final GatewayDiscordClient client = DiscordClientBuilder.create(token).build().login().block();
 
-		// bind PORT for Heroku integration
-		HerokuServer.create(Integer.parseInt(System.getenv("PORT")));
+		// we should only find this when running on Heroku
+		String port = System.getenv("PORT");
+		if (port != null) {
+			// bind PORT for Heroku integration
+			HerokuServer.create(Integer.parseInt(port));
+		} else {
+			LOGGER.info("Not running on Heroku");
+		}
 
 		CommandClient.create(client);
 
