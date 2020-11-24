688a64739192c345bf3d15f3f02599cded0249b4
reptar25 <bailey.kevin89@gmail.com>
2020-11-24 11:05:13 -0800

    Removed import

--- MudPitBot/src/main/java/com/github/MudPitBot/botCommand/sound/TrackScheduler.java
+++ MudPitBot/src/main/java/com/github/MudPitBot/botCommand/sound/TrackScheduler.java
@@ -1,7 +1,6 @@
 package com.github.MudPitBot.botCommand.sound;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.BlockingQueue;
