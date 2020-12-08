500d8c697fa1994413e8d2e1b51d4ed96658f30a
reptar25 <bailey.kevin89@gmail.com>
2020-12-07 20:46:57 -0800

    Create invite path and invite link on root page

--- MudPitBot/src/main/java/com/github/MudPitBot/heroku/HerokuServer.java
+++ MudPitBot/src/main/java/com/github/MudPitBot/heroku/HerokuServer.java
@@ -29,7 +29,7 @@ public class HerokuServer {
 //		try {
 		server = HttpServer.create(new InetSocketAddress("0.0.0.0", port), 0);
 		server.createContext("/", new RootResponseHandler());
-		server.createContext("/hello", new HelloResponseHandler());
+		server.createContext("/invite", new InviteResponseHandler());
 		server.setExecutor(null);
 		server.start();
 		System.out.println("Server started on port " + port);
@@ -42,8 +42,9 @@ public class HerokuServer {
 }
 
 class RootResponseHandler implements HttpHandler {
-	private static final String homeHtml = "<h1 style='font-family: sans-serif;'>Hey</h1>"
-			+ "<img style=\"-webkit-user-select: none;margin: auto;cursor: zoom-in;\""
+	private static final String INVITE_LINK = "https://discord.com/api/oauth2/authorize?client_id=776639160164941824&permissions=8&scope=bot";
+	private static final String homeHtml = "<a href=\"" + INVITE_LINK + "\">Invite!</a>"
+			+ "<img style=\"-webkit-user-select: none;margin: auto;\""
 			+ " src=\"https://i.ytimg.com/vi/4Qto049GEkA/maxresdefault.jpg\" width=\"235\" height=\"132\">";
 
 	public void handle(HttpExchange exchange) throws IOException {
@@ -60,17 +61,20 @@ class RootResponseHandler implements HttpHandler {
 	}
 }
 
-class HelloResponseHandler implements HttpHandler {
-	private static final String homeHtml = "<h1 style='font-family: sans-serif;'>HELLO</h1>";
+class InviteResponseHandler implements HttpHandler {
+	private static final String INVITE_LINK = "https://discord.com/api/oauth2/authorize?client_id=776639160164941824&permissions=8&scope=bot";
+	private static final String inviteHtml = "<html>\r\n" + "   <head>\r\n" + "      <title>HTML Meta Tag</title>\r\n"
+			+ "      <meta http-equiv = \"refresh\" content = \"0; url = " + INVITE_LINK + "\" />\r\n"
+			+ "   </head>\r\n" + "   <body>\r\n" + "\r\n" + "   </body>\r\n" + "</html>";
 
 	public void handle(HttpExchange exchange) throws IOException {
 //			Headers h = exchange.getRequestHeaders();
 //			h.add("Content-Type", "text/html");
 
 		InputStream is = exchange.getRequestBody();
-		exchange.sendResponseHeaders(200, homeHtml.length());
+		exchange.sendResponseHeaders(200, inviteHtml.length());
 		OutputStream os = exchange.getResponseBody();
-		os.write(homeHtml.getBytes());
+		os.write(inviteHtml.getBytes());
 		os.flush();
 		os.close();
 		is.close();
