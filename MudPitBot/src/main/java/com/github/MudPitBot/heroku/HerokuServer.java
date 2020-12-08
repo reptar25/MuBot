a9b0326e261a21c7bcb216f239300ebae054228b
reptar25 <bailey.kevin89@gmail.com>
2020-12-07 20:21:02 -0800

    Change webserver root site

--- MudPitBot/src/main/java/com/github/MudPitBot/heroku/HerokuServer.java
+++ MudPitBot/src/main/java/com/github/MudPitBot/heroku/HerokuServer.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
@@ -14,25 +16,25 @@ public class HerokuServer {
 	private HttpServer server;
 	private static HerokuServer instance;
 
-	public static HerokuServer create(int port) {
+	public static HerokuServer create(int port) throws IOException {
 		if (instance == null)
 			instance = new HerokuServer(port);
 
 		return instance;
 	}
 
-	private HerokuServer(int port) {
+	private HerokuServer(int port) throws IOException {
 		this.port = port;
 
-		try {
-			server = HttpServer.create(new InetSocketAddress("0.0.0.0", port), 0);
-			server.createContext("/", new RootResponseHandler());
-			server.setExecutor(null);
-			server.start();
-			System.out.println("Server started on port " + port);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+//		try {
+		server = HttpServer.create(new InetSocketAddress("0.0.0.0", port), 0);
+		server.createContext("/", new RootResponseHandler());
+		server.setExecutor(null);
+		server.start();
+		System.out.println("Server started on port " + port);
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
 
 	}
 
@@ -44,6 +46,9 @@ class RootResponseHandler implements HttpHandler {
 			+ " src=\"https://i.ytimg.com/vi/4Qto049GEkA/maxresdefault.jpg\" width=\"235\" height=\"132\">";
 
 	public void handle(HttpExchange exchange) throws IOException {
+		Headers h = exchange.getRequestHeaders();
+		h.add("Content-Type", "text/html");
+
 		InputStream is = exchange.getRequestBody();
 		exchange.sendResponseHeaders(200, homeHtml.length());
 		OutputStream os = exchange.getResponseBody();
