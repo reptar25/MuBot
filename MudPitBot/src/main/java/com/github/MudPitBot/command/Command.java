283ac0c0cfc61855fca83f8cb50179b5ae16e65b
reptar25 <bailey.kevin89@gmail.com>
2020-12-14 13:23:45 -0800

    Added requireVoiceChannel and requireSameVocieChannel methods to Command

--- MudPitBot/src/main/java/com/github/MudPitBot/command/Command.java
+++ MudPitBot/src/main/java/com/github/MudPitBot/command/Command.java
@@ -1,18 +1,25 @@
 package com.github.MudPitBot.command;
 
+import java.util.Optional;
+
 import com.github.MudPitBot.sound.TrackScheduler;
 
+import discord4j.common.util.Snowflake;
 import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.VoiceState;
 import discord4j.core.object.entity.Member;
+import discord4j.core.object.entity.channel.VoiceChannel;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.Logger;
+import reactor.util.Loggers;
 
 /**
  * Implementation of the Command design pattern.
  */
 
 public abstract class Command implements CommandInterface {
-
+	private static final Logger LOGGER = Loggers.getLogger(Command.class);
 	protected String commandTrigger;
 
 	public Command(String commandTrigger) {
@@ -39,17 +46,64 @@ public abstract class Command implements CommandInterface {
 	 */
 	private static final int RETRY_AMOUNT = 100;
 
-	protected static Mono<TrackScheduler> getScheduler(MessageCreateEvent event) {
-		// MessageChannel messageChannel = event.getMessage().getChannel().block();
-		return Mono.justOrEmpty(event.getGuildId()).flatMap(guildId -> {
-			return event.getClient().getMemberById(guildId, event.getClient().getSelfId())
-					.flatMap(Member::getVoiceState).flatMap(s -> Mono.justOrEmpty(s.getChannelId()))
-					.flatMap(channelId -> {
-						// repeat until we find the scheduler
-						// allows things like !join !play to work
-						return Mono.justOrEmpty(TrackScheduler.getScheduler(channelId.asLong()))
-								.repeatWhenEmpty(RETRY_AMOUNT, Flux::repeat);
-					});
-		});
+	protected static Mono<TrackScheduler> getScheduler(VoiceChannel channel) {
+		return Mono.justOrEmpty(TrackScheduler.getScheduler(channel.getId().asLong())).repeatWhenEmpty(RETRY_AMOUNT,
+				Flux::repeat);
+	}
+
+	/**
+	 * Returns the voice channel the message sender is in or empty if they are not
+	 * in a voice channel
+	 * 
+	 * @param event the message event
+	 * @return the voice channel of the message sender
+	 */
+	protected static Mono<VoiceChannel> requireVoiceChannel(MessageCreateEvent event) {
+		return Mono.justOrEmpty(event.getMember()).flatMap(Member::getVoiceState).map(VoiceState::getChannelId)
+				.defaultIfEmpty(Optional.empty()).flatMap(s -> {
+					if (s.isPresent())
+						return Mono.just(s.get());
+
+					LOGGER.info("User is not in a voice channel");
+					return Mono.empty();
+				}).flatMap(event.getClient()::getChannelById).cast(VoiceChannel.class);
+	}
+
+	/**
+	 * 
+	 * @param event the message event
+	 * @return the voice channel the bot and message sender share or empty if they
+	 *         do not share a voice channel
+	 */
+	protected static Mono<VoiceChannel> requireSameVoiceChannel(MessageCreateEvent event) {
+		// id of the bot's voice channel id or empty
+		final Mono<Optional<Snowflake>> getBotVoiceChannelId = event.getClient().getSelf()
+				.flatMap(user -> user.asMember(event.getGuildId().orElseThrow())).flatMap(Member::getVoiceState)
+				.map(VoiceState::getChannelId).defaultIfEmpty(Optional.empty());
+
+		// id of the user's voice channel id or empty
+		final Mono<Optional<Snowflake>> getUserVoiceChannelId = Mono.justOrEmpty(event.getMember())
+				.flatMap(Member::getVoiceState).map(VoiceState::getChannelId).defaultIfEmpty(Optional.empty());
+
+		return Mono.zip(getBotVoiceChannelId, getUserVoiceChannelId).map(tuple -> {
+			final Optional<Snowflake> botVoiceChannelId = tuple.getT1();
+			final Optional<Snowflake> userVoiceChannelId = tuple.getT2();
+
+			// If the user and the bot are not in a voice channel
+			if (botVoiceChannelId.isEmpty() && userVoiceChannelId.isEmpty()) {
+				LOGGER.info("User and bot are not in a voice channel");
+				return Snowflake.of(-1l);
+			}
+
+			// If the user and the bot are not in the same voice channel
+			if (botVoiceChannelId.isPresent()
+					&& !userVoiceChannelId.map(botVoiceChannelId.get()::equals).orElse(false)) {
+				LOGGER.info("User and bot are not in the same voice channel");
+				return Snowflake.of(-1l);
+			}
+
+			return userVoiceChannelId.get();
+		}).filter(snowflake -> snowflake.asLong() != -1).flatMap(event.getClient()::getChannelById)
+				.cast(VoiceChannel.class);
 	}
 }
