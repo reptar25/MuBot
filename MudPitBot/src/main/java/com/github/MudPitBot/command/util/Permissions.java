c90bfe9bea0cd9ccbef23c7c70aa7aaac8989428
reptar25 <bailey.kevin89@gmail.com>
2021-01-05 12:17:38 -0800

    Moved checkPermissions into its own method

--- MudPitBot/src/main/java/com/github/MudPitBot/command/util/Permissions.java
+++ MudPitBot/src/main/java/com/github/MudPitBot/command/util/Permissions.java
@@ -88,85 +88,6 @@ public final class Permissions {
 	}
 
 	/**
-	 * 
-	 * @param channel              the guild channel
-	 * @param requestedPermissions the permissions the bot will need
-	 * @return the permissions the bot has in this channel or an error if the bot
-	 *         does not have the requested permissions
-	 */
-	public static Mono<PermissionSet> requireBotPermissions(GuildChannel channel, Permission... requestedPermissions) {
-		return channel.getEffectivePermissions(channel.getClient().getSelfId()).flatMap(permissions -> {
-			for (Permission permission : requestedPermissions) {
-				if (!permissions.contains(permission)) {
-					StringBuilder sb = new StringBuilder("I don't have permission to");
-					RuntimeException exception = new BotPermissionException(
-							new StringBuilder("Bot missing ").append(permission).append(" for command").toString());
-					switch (permission) {
-					case CONNECT:
-						((BotPermissionException) exception)
-								.setUserFriendlyMessage(sb.append(" connect to ").append(channel.getName()).toString());
-						break;
-					case SPEAK:
-						((BotPermissionException) exception)
-								.setUserFriendlyMessage(sb.append(" speak in ").append(channel.getName()).toString());
-						break;
-					case VIEW_CHANNEL:
-						((BotPermissionException) exception)
-								.setUserFriendlyMessage(sb.append(" view ").append(channel.getName()).toString());
-						break;
-					case MUTE_MEMBERS:
-						((BotPermissionException) exception).setUserFriendlyMessage(
-								sb.append(" mute members in ").append(channel.getName()).toString());
-						break;
-					case SEND_MESSAGES:
-						exception = new SendMessagesException(
-								new StringBuilder("Bot missing ").append(permission).append(" for command").toString());
-						break;
-					case ADD_REACTIONS:
-						((BotPermissionException) exception).setUserFriendlyMessage(
-								sb.append(" add message reactions in ").append(channel.getName()).toString());
-						break;
-					case MANAGE_MESSAGES:
-						((BotPermissionException) exception).setUserFriendlyMessage(
-								sb.append(" manage messages in ").append(channel.getName()).toString());
-						break;
-					case ADMINISTRATOR:
-					case ATTACH_FILES:
-					case BAN_MEMBERS:
-					case CHANGE_NICKNAME:
-					case CREATE_INSTANT_INVITE:
-					case DEAFEN_MEMBERS:
-					case EMBED_LINKS:
-					case KICK_MEMBERS:
-					case MANAGE_CHANNELS:
-					case MANAGE_EMOJIS:
-					case MANAGE_GUILD:
-					case MANAGE_NICKNAMES:
-					case MANAGE_ROLES:
-					case MANAGE_WEBHOOKS:
-					case MENTION_EVERYONE:
-					case MOVE_MEMBERS:
-					case PRIORITY_SPEAKER:
-					case READ_MESSAGE_HISTORY:
-					case SEND_TTS_MESSAGES:
-					case STREAM:
-					case USE_EXTERNAL_EMOJIS:
-					case USE_VAD:
-					case VIEW_AUDIT_LOG:
-					case VIEW_GUILD_INSIGHTS:
-					default:
-						return Mono.error(new BotPermissionException(
-								sb.append(" do that in ").append(channel.getName()).toString()));
-					}
-
-					return Mono.error(exception);
-				}
-			}
-			return Mono.just(permissions);
-		});
-	}
-
-	/**
 	 * Checks if the message was sent in a private message or not
 	 * 
 	 * @param event the MessageCreateEvent
@@ -176,5 +97,102 @@ public final class Permissions {
 		return Mono.justOrEmpty(event.getMember())
 				.switchIfEmpty(Mono.error(new CommandException("Voice command in private message",
 						"You can't use this command in a private message")));
+	}
+
+	/**
+	 * 
+	 * @param channel              the guild channel
+	 * @param requestedPermissions the permissions the bot will need
+	 * @return the permissions the bot has in this channel or an error if the bot
+	 *         does not have the requested permissions
+	 */
+
+	public static Mono<PermissionSet> requireBotPermissions(GuildChannel channel, Permission... requestedPermissions) {
+		return channel.getEffectivePermissions(channel.getClient().getSelfId()).flatMap(permissions -> {
+			return checkPermissions(channel.getName(), permissions, requestedPermissions);
+		});
+	}
+
+	/**
+	 * Checks the given permissions set agains the requested permission
+	 * 
+	 * @param channelName          the name of the channel permissions are being
+	 *                             checked for
+	 * @param permissions          the set of permissions that the user has for the
+	 *                             given channel
+	 * @param requestedPermissions the permissions the user has requested for this
+	 *                             channel
+	 * @return the PermissionSet of the user for this channel, or error if the user
+	 *         doesn't have one of the requested permissions for this channel
+	 */
+
+	private static Mono<PermissionSet> checkPermissions(String channelName, PermissionSet permissions,
+			Permission... requestedPermissions) {
+		for (Permission permission : requestedPermissions) {
+			if (!permissions.contains(permission)) {
+				StringBuilder sb = new StringBuilder("I don't have permission to");
+				RuntimeException exception = new BotPermissionException(
+						new StringBuilder("Bot missing ").append(permission).append(" for command").toString());
+				switch (permission) {
+				case CONNECT:
+					((BotPermissionException) exception)
+							.setUserFriendlyMessage(sb.append(" connect to ").append(channelName).toString());
+					break;
+				case SPEAK:
+					((BotPermissionException) exception)
+							.setUserFriendlyMessage(sb.append(" speak in ").append(channelName).toString());
+					break;
+				case VIEW_CHANNEL:
+					((BotPermissionException) exception)
+							.setUserFriendlyMessage(sb.append(" view ").append(channelName).toString());
+					break;
+				case MUTE_MEMBERS:
+					((BotPermissionException) exception)
+							.setUserFriendlyMessage(sb.append(" mute members in ").append(channelName).toString());
+					break;
+				case SEND_MESSAGES:
+					exception = new SendMessagesException(
+							new StringBuilder("Bot missing ").append(permission).append(" for command").toString());
+					break;
+				case ADD_REACTIONS:
+					((BotPermissionException) exception).setUserFriendlyMessage(
+							sb.append(" add message reactions in ").append(channelName).toString());
+					break;
+				case MANAGE_MESSAGES:
+					((BotPermissionException) exception)
+							.setUserFriendlyMessage(sb.append(" manage messages in ").append(channelName).toString());
+					break;
+				case ADMINISTRATOR:
+				case ATTACH_FILES:
+				case BAN_MEMBERS:
+				case CHANGE_NICKNAME:
+				case CREATE_INSTANT_INVITE:
+				case DEAFEN_MEMBERS:
+				case EMBED_LINKS:
+				case KICK_MEMBERS:
+				case MANAGE_CHANNELS:
+				case MANAGE_EMOJIS:
+				case MANAGE_GUILD:
+				case MANAGE_NICKNAMES:
+				case MANAGE_ROLES:
+				case MANAGE_WEBHOOKS:
+				case MENTION_EVERYONE:
+				case MOVE_MEMBERS:
+				case PRIORITY_SPEAKER:
+				case READ_MESSAGE_HISTORY:
+				case SEND_TTS_MESSAGES:
+				case STREAM:
+				case USE_EXTERNAL_EMOJIS:
+				case USE_VAD:
+				case VIEW_AUDIT_LOG:
+				case VIEW_GUILD_INSIGHTS:
+				default:
+					return Mono.error(
+							new BotPermissionException(sb.append(" do that in ").append(channelName).toString()));
+				}
+				return Mono.error(exception);
+			}
+		}
+		return Mono.just(permissions);
 	}
 }
