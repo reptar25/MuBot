c5969ec72eaa038ee2633f84ab435c86d2cb7a26
reptar25 <bailey.kevin89@gmail.com>
2021-01-08 13:07:48 -0800

    Slightly rework PollMenu

--- MudPitBot/src/main/java/com/github/MudPitBot/command/menu/menus/PollMenu.java
+++ MudPitBot/src/main/java/com/github/MudPitBot/command/menu/menus/PollMenu.java
@@ -1,11 +1,12 @@
 package com.github.MudPitBot.command.menu.menus;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
+import static com.github.MudPitBot.command.util.CommandUtil.DEFAULT_EMBED_COLOR;
+import java.time.Instant;
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Consumer;
-
+import java.util.stream.Collectors;
 import com.github.MudPitBot.command.menu.Menu;
 import com.github.MudPitBot.command.util.Emoji;
 
@@ -13,55 +14,20 @@ import discord4j.core.object.entity.Member;
 import discord4j.core.object.entity.Message;
 import discord4j.core.spec.EmbedCreateSpec;
 import discord4j.core.spec.MessageCreateSpec;
-import discord4j.rest.util.Color;
 import reactor.util.Logger;
 import reactor.util.Loggers;
 
-/**
- * Data class for polls.
- */
 public final class PollMenu extends Menu {
 
-	/**
-	 * Builder for polls. Not really needed now, but can be used to expand polls
-	 * easier later on.
-	 */
-//	public static class Builder {
-//
-//		private MessageCreateEvent event;
-//		private String[] args;
-//		private String footer;
-//		private String footerURL;
-//		private String title;
-//		private ArrayList<String> answers = new ArrayList<String>();
-//		private String description;
-//
-//		public Builder(MessageCreateEvent event) {
-//			this.event = event;
-//		}
-//
-//		public Poll build() {
-//			return new Poll(this);
-//		}
-//	}
-
 	private static final Logger LOGGER = Loggers.getLogger(PollMenu.class);
+	private final int MAX_ANSWERS = 11;
 	private String[] args;
 	private Member member;
-	private String footer;
-	private String footerURL;
+	private String authorName;
+	private String authorIcon;
 	private String title;
 	private ArrayList<String> answers = new ArrayList<String>();
 	private String description;
-
-//	private Poll(Builder builder) {
-//		// this.event = builder.event;
-//		this.footer = builder.footer;
-//		this.footerURL = builder.footerURL;
-//		this.title = builder.title;
-//		this.answers = builder.answers;
-//		this.description = builder.description;
-//	}
 
 	public PollMenu(String[] args, Member member) {
 		this.args = args;
@@ -83,47 +49,52 @@ public final class PollMenu extends Menu {
 
 	private void createPoll() {
 
-		createAnswers(args);
+		setAnswers(args);
 
 		if (answers.isEmpty())
 			return;
 
-		createFooter();
+		setAuthor();
 
-		createDescription();
+		setDescription();
 	}
 
-	private void createAnswers(String[] args) {
-		// String[] splitCommand = command.split(" \"");
-
+	private void setAnswers(String[] args) {
 		if (args == null || args.length < 3) {
 			LOGGER.info("Not enough arguments for poll command");
 			return;
 		}
-
-		title = args[0];
-
-		for (int i = 1; i < args.length; i++) {
-			answers.add(args[i]);
+		// only allow of 1 question and 10 answers as arguments
+		List<String> arguments = Arrays.stream(args).limit(MAX_ANSWERS).collect(Collectors.toList());
+		title = arguments.get(0);
+		for (int i = 1; i < arguments.size(); i++) {
+			answers.add(arguments.get(i));
 		}
 	}
 
-	private void createFooter() {
-		Date date = new Date(System.currentTimeMillis());
-		DateFormat df = new SimpleDateFormat("EEEE, MMMM dd");// , 'at' hh:mm a
-		String timeStamp = df.format(date);
-
-		StringBuilder sb = new StringBuilder("Poll created by ");
-		if (member != null)
-			sb.append(member.getUsername());
-		sb.append(" on ");
-		sb.append(timeStamp);
-		this.footer = sb.toString();
-		if (member != null)
-			this.footerURL = member.getAvatarUrl();
+	private void setAuthor() {
+		if (member != null) {
+			this.authorName = member.getUsername();
+			this.authorIcon = member.getAvatarUrl();
+		}
 	}
 
-	private void createDescription() {
+//	private void createFooter() {
+//		Date date = new Date(System.currentTimeMillis());
+//		DateFormat df = new SimpleDateFormat("EEEE, MMMM dd");// , 'at' hh:mm a
+//		String timeStamp = df.format(date);
+//
+//		StringBuilder sb = new StringBuilder("Poll created by ");
+//		if (member != null)
+//			sb.append(member.getUsername());
+//		sb.append(" on ");
+//		sb.append(timeStamp);
+//		this.author = sb.toString();
+//		if (member != null)
+//			this.authorIcon = member.getAvatarUrl();
+//	}
+
+	private void setDescription() {
 		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < answers.size(); i++) {
 			String answer = answers.get(i);
@@ -144,7 +115,8 @@ public final class PollMenu extends Menu {
 	}
 
 	private Consumer<? super EmbedCreateSpec> createEmbed() {
-		return embed -> embed.setColor(Color.of(23, 53, 77)).setFooter(footer, footerURL).setDescription(description);
+		return embed -> embed.setColor(DEFAULT_EMBED_COLOR).setFooter("Poll created by " + authorName, authorIcon)
+				.setTimestamp(Instant.now()).setDescription(description);
 	}
 
 }
