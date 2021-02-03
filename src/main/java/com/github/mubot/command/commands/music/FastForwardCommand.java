8f24168af2cfc2ba523b7a536dabdfb9e790c589
reptar25 <bailey.kevin89@gmail.com>
2021-02-03 14:12:32 -0800

    Abstract track seeking commands

--- src/main/java/com/github/mubot/command/commands/music/FastForwardCommand.java
+++ src/main/java/com/github/mubot/command/commands/music/FastForwardCommand.java
@@ -3,46 +3,18 @@ package com.github.mubot.command.commands.music;
 import java.util.Arrays;
 import java.util.function.Consumer;
 
-import com.github.mubot.command.CommandResponse;
 import com.github.mubot.command.help.CommandHelpSpec;
 import com.github.mubot.music.TrackScheduler;
 
-import discord4j.core.event.domain.message.MessageCreateEvent;
-import discord4j.core.object.entity.channel.VoiceChannel;
-import reactor.core.publisher.Mono;
-import reactor.util.annotation.NonNull;
-
-public class FastForwardCommand extends MusicCommand {
+public class FastForwardCommand extends TrackSeekingCommand {
 
 	public FastForwardCommand() {
 		super("fastforward", Arrays.asList("ff"));
 	}
 
 	@Override
-	protected Mono<CommandResponse> action(MessageCreateEvent event, String[] args, TrackScheduler scheduler,
-			VoiceChannel channel) {
-		return fastForward(event, args, scheduler);
-	}
-
-	/**
-	 * @param event
-	 * @param event The message event
-	 * @param args  The amount of time in seconds to fast forward
-	 * @return null
-	 */
-	public Mono<CommandResponse> fastForward(MessageCreateEvent event, @NonNull String[] args,
-			@NonNull TrackScheduler scheduler) {
-		if (args.length > 0) {
-			try {
-				int amountInSeconds = Integer.parseInt(args[0]);
-				scheduler.fastForward(amountInSeconds);
-				return Mono.empty();
-			} catch (NumberFormatException e) {
-				// just ignore commands with improper number
-				return Mono.empty();
-			}
-		}
-		return getHelp(event);
+	protected void doSeeking(TrackScheduler scheduler, int amountInSeconds) {
+		scheduler.fastForward(amountInSeconds);
 	}
 
 	@Override
@@ -50,5 +22,4 @@ public class FastForwardCommand extends MusicCommand {
 		return spec -> spec.setDescription("Fast fowards the currently playing song by the given amount of seconds.")
 				.addArg("time", "amount of time in seconds to fast foward", false).addExample("60");
 	}
-
 }
--- src/main/java/com/github/mubot/command/commands/music/SeekCommand.java
+++ src/main/java/com/github/mubot/command/commands/music/SeekCommand.java
@@ -2,44 +2,18 @@ package com.github.mubot.command.commands.music;
 
 import java.util.function.Consumer;
 
-import com.github.mubot.command.CommandResponse;
 import com.github.mubot.command.help.CommandHelpSpec;
 import com.github.mubot.music.TrackScheduler;
 
-import discord4j.core.event.domain.message.MessageCreateEvent;
-import discord4j.core.object.entity.channel.VoiceChannel;
-import reactor.core.publisher.Mono;
-import reactor.util.annotation.NonNull;
-
-public class SeekCommand extends MusicCommand {
+public class SeekCommand extends TrackSeekingCommand {
 
 	public SeekCommand() {
 		super("seek");
 	}
 
 	@Override
-	protected Mono<CommandResponse> action(MessageCreateEvent event, String[] args, TrackScheduler scheduler,
-			VoiceChannel channel) {
-		return seek(event, args, scheduler);
-	}
-
-	/**
-	 * @param event
-	 * @param event The message event
-	 * @param args  The position to move the current song to in seconds
-	 * @return null
-	 */
-	public Mono<CommandResponse> seek(MessageCreateEvent event, @NonNull String[] args,
-			@NonNull TrackScheduler scheduler) {
-		if (args.length > 0) {
-			try {
-				int positionInSeconds = Integer.parseInt(args[0]);
-				scheduler.seek(positionInSeconds);
-			} catch (NumberFormatException e) {
-				return getHelp(event);
-			}
-		}
-		return getHelp(event);
+	protected void doSeeking(TrackScheduler scheduler, int amountInSeconds) {
+		scheduler.seek(amountInSeconds);
 	}
 
 	@Override
